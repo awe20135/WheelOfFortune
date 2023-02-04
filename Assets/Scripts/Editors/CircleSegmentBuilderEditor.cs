15a08266c55645f6d65e6477ad40ddd26fbde030
awe20135 <awe20135@gmail.com>
2023-02-04 13:27:52 +0200

    Added UNITY_EDITOR definition.

--- Assets/Scripts/Editors/CircleSegmentBuilderEditor.cs
+++ Assets/Scripts/Editors/CircleSegmentBuilderEditor.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using WheelOfFortune.Utilities;
@@ -22,3 +21,4 @@ namespace WheelOfFortune.Editors
         }
     }
 }
+#endif
